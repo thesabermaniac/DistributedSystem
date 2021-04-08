711499afba95f5d55974bedeaed31dd111d250b7
RumblyElk <rumblyelk@users.noreply.github.com>
2021-04-08 16:39:43 +0300

    more or less finished the slave and job classes

---
+++ src/main/Job.java
@@ -0,0 +1,28 @@
+package main;
+
+public class Job {
+    private final String jobType;
+    private final int id;
+    private boolean isFinished = false;
+
+    public Job(String jobType, int id) {
+        this.jobType = jobType;
+        this.id = id;
+    }
+
+    public String getJobType() {
+        return jobType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setIsFinished(boolean isFinished) {
+        this.isFinished = isFinished;
+    }
+
+    public boolean getIsFinished() {
+        return isFinished;
+    }
+}
--- src/main/Slave.java
+++ src/main/Slave.java
@@ -6,32 +6,35 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Slave {
     private final String slaveType;
     private Socket socket;
-    private ServerSocket serverSocket;
     private ObjectInputStream in;
     private DataOutputStream out;
-    // private ArrayList<Job> jobs;
+    private final LinkedList<Job> jobs;
 
     Slave(String slaveType) {
         this.slaveType = slaveType;
+        jobs = new LinkedList<>();
         connectToMaster();
     }
 
     private void connectToMaster() {
         try {
-            serverSocket = new ServerSocket(5000);
-            System.out.println("SLAVE " + slaveType + ": Awaiting connection to MASTER...");
+            ServerSocket serverSocket = new ServerSocket(5000);
+            System.out.println(slaveTypeToString() + ": Awaiting connection to MASTER...");
 
             socket = serverSocket.accept();
-            System.out.println("SLAVE " + slaveType + ": Connection to MASTER established.");
+            System.out.println(slaveTypeToString() + ": Connection to MASTER established.");
 
             in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
             out = new DataOutputStream(socket.getOutputStream());
-        } catch (IOException unknownHostException) {
-            unknownHostException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
         }
     }
 
@@ -40,26 +43,101 @@ public class Slave {
             in.close();
             out.close();
             socket.close();
-            System.out.println("SLAVE " + slaveType + ": Disconnected from MASTER.");
+            System.out.println(slaveTypeToString() + ": Disconnected from MASTER.");
         } catch (IOException ioException) {
-            System.out.println(ioException.toString());
+            ioException.printStackTrace();
         }
     }
 
-    private void acceptJob() {
-        // TODO
+    private void sendMessageToMaster(String message) {
+        try {
+            out.writeUTF(message);
+            out.flush();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    private void receiveJob() {
+        try {
+            Job newJob = (Job) in.readObject();
+            synchronized (jobs) {
+                jobs.add(newJob);
+            }
+        } catch (IOException | ClassNotFoundException ioException) {
+            ioException.printStackTrace();
+        }
     }
 
     private void doJob() {
         // TODO
+
+        // make sure the queue's not empty
+        if (jobs.isEmpty()) {
+            sendMessageToMaster(slaveTypeToString() + ": This slave has no jobs to finish.");
+            return;
+        }
+
         // get next job in queue
-        // if it's this.slaveType, sleep thread for 2 seconds
-        // if it's not the type, sleep thread for 10 seconds
+        AtomicReference<Job> curJob = new AtomicReference<>();
+
+        new Thread(() -> {
+            synchronized (jobs) {
+                curJob.set(jobs.peekFirst());
+            }
+
+            try {
+                if (curJob.get() != null) {
+                    if (curJob.get().getJobType().equals(slaveType)) {
+                        // this is a job of the same type and it is therefore "optimized"
+                        Thread.sleep(2000);
+                    } else {
+                        // this is a job of a different type and it is therefore "unoptimized"
+                        Thread.sleep(10_000);
+                    }
+                }
+
+                // make sure to remove the finished job
+                synchronized (jobs) {
+                    jobs.removeFirst();
+                }
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+        });
+
         // send a message back and say the job was finished
+        sendMessageToMaster(slaveTypeToString() + "Job ID " + curJob + " has been completed.");
     }
 
+    public int computeTimeTillAllJobsFinished() {
+        AtomicInteger total = new AtomicInteger();
+
+        if (jobs.isEmpty()) {
+            sendMessageToMaster(slaveTypeToString() + ": This slave has no jobs to finish.");
+            return total.get();
+        }
+
+        new Thread(() -> {
+            synchronized (jobs) {
+                for (Job job : jobs) {
+                    if (job.getJobType().equals(slaveType)) {
+                        total.addAndGet(2);
+                    } else {
+                        total.addAndGet(10);
+                    }
+                }
+            }
+        });
+
+        return total.get();
+    }
 
     public String getSlaveType() {
         return slaveType;
+    }
+
+    private String slaveTypeToString() {
+        return "SLAVE-" + slaveType;
     }
 }
